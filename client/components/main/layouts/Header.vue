28c702243ba89c5ee9ff593d0b7e2e0652128215
dm1annem <300225@mail.ru>
2021-05-28 18:59:43 +0300

    first commit

---
+++ client/components/main/layouts/Header.vue
@@ -0,0 +1,33 @@
+<template>
+  <nav>
+    <v-app-bar app >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-app-bar-title>
+        <span>Каркас</span>
+        <span>Центр</span>
+      </v-app-bar-title>
+    </v-app-bar>
+    
+    <v-navigation-drawer
+      v-model="drawer"
+      absolute
+      temporary
+      />
+    
+  </nav>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      drawer: false,
+    }
+  },
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ client/layouts/default.vue
@@ -0,0 +1,30 @@
+<template>
+<v-app>
+  <my-header/>
+
+  <v-main >
+    <v-container >
+      <Nuxt />
+    </v-container>
+  </v-main>
+
+  <my-footer/>
+</v-app>
+</template>
+
+<script>
+import MyHeader from '@/components/main/layouts/Header.vue'
+import MyFooter from '@/components/main/layouts/Footer'
+export default {
+  components:{
+        MyHeader,
+        MyFooter,
+  },
+  data () {
+    return {
+      
+      
+    }
+  }
+}
+</script>
