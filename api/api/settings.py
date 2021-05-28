a92a4072611eebca0e9e0c82b61a88ef9f43c952
dm1annem <300225@mail.ru>
2021-05-28 19:02:12 +0300

    first commit 2

--- api/api/settings.py
+++ api/api/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-nd3_p1zaul1g)f#quo3g@r^n+crre2i@^qz-9ea9#&rn+fqrcw'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
